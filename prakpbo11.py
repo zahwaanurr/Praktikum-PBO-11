6621847e848e0335a4b239da5b1adef16115bf4f
zahwaanurr <145866169+zahwaanurr@users.noreply.github.com>
2024-05-23 23:42:29 +0700

    Add files via upload

--- prakpbo11.py
+++ prakpbo11.py
@@ -1,19 +1,19 @@
 import functools
 
-def display_info_decorator(func):
-    @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
-        full_name = func(self, *args, **kwargs)
-        return f"Full Name: {full_name} {self.nim}"
-    return wrapper
-
 class Person:
     def __init__(self, first_name, last_name, title, nim):
         self.first_name = first_name
         self.last_name = last_name
         self.title = title
         self.nim = nim
-
+        
+    def display_info_decorator(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            full_name = func(self, *args, **kwargs)
+            return f"Full Name: {full_name} {self.nim}"
+        return wrapper
+   
     @display_info_decorator
     def get_full_name(self):
         return f"{self.title} {self.first_name} {self.last_name}"
